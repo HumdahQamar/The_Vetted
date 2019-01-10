e19cbe7d0fb92f5177784d409f7497b5956da98e
HumdahQamar <hqmannan@gmail.com>
2019-01-10 17:44:48 +0500

    Create basic model

--- the_vetted/roster/models.py
+++ the_vetted/roster/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=20)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class User(AbstractUser):
+    bio = models.CharField(max_length=200)
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True)
+
+    USER_TYPE_CHOICES = (
+        (1, 'employee'),
+        (2, 'admin'),
+        (3, 'super_user')
+    )
+
+    type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
+
+    def __str__(self):
+        return self.username
+
