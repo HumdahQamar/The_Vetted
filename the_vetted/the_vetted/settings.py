1ff171b60696cfed18bf4e4e3bd94e92afba9152
HumdahQamar <hqmannan@gmail.com>
2019-01-25 21:57:44 +0500

    Attempt deployment #2

--- the_vetted/the_vetted/settings.py
+++ the_vetted/the_vetted/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_x(hvp5m!7nzr&ey9kook%n(s_%8)kt1lxnovpk#rw@jfd)dv&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver', 'heroku-django-roster.herokuapp.com']
+ALLOWED_HOSTS = ['roster-django.herokuapp.com']
 
 
 # Application definition
